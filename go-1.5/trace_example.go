03e91e1dad2388b6cbef961a45fbf5dab05d4730
Leslie <zhangxin@aiyun.com.cn>
2016-02-18 16:52:58 +0800

    first commit

---
+++ go-1.5/trace_example.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"math/rand"
+	"os"
+	"os/signal"
+	"runtime/trace"
+	"sort"
+	"sync"
+	"syscall"
+)
+
+var aList = make([]string, 0)
+var aMutex = new(sync.Mutex)
+
+func main() {
+	f, err := os.Create("trace_example_trace.out")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer f.Close()
+	trace.Start(f)
+	defer trace.Stop()
+
+	for i := 0; i < 5; i++ {
+		go doSomething()
+	}
+
+	sigc := make(chan os.Signal)
+	signal.Notify(sigc, syscall.SIGINT, syscall.SIGTERM)
+	log.Printf("Got singal '%v' .. exit now", <-sigc)
+}
+
+func doSomething() {
+	for {
+		aMutex.Lock()
+		for i := 0; i < 10; i++ {
+			aList = append(aList, randSeq(50))
+		}
+		sort.Strings(aList)
+		if len(aList) > 10000 {
+			aList = aList[0:10000]
+		}
+		log.Printf("list len %v", len(aList))
+		aMutex.Unlock()
+	}
+}
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randSeq(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
